6fc7deb27aff9412a29cf694f99b7365ae2fd3f7
castromx <96194271+castromx@users.noreply.github.com>
2023-04-21 20:10:09 +0300

    Update settings.py

--- zodsign/zodsign/settings.py
+++ zodsign/zodsign/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uz3^1x2qi75qco6dw@7ypvk4o$3fo4-&&zfa+793=dyah3sta1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
